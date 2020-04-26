8d0d4162bf38afa1de754e82fdd9679621323183
tompkind <tompkins.daniel@gmail.com>
2020-04-26 14:35:11 -0400

    Week 1

---
+++ plot1.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/TompkinsD/Documents/coursera2019/r4")
+
+power <- read.csv("C:/Users/TompkinsD/Documents/coursera2019/r4/household_power_consumption.txt", sep=";", na.strings=c("?"))
+power$DateTime <- paste(power$Date,power$Time)
+power$DateTime <- strptime(power$DateTime, "%d/%m/%Y %H:%M:%S")
+power$Date <- as.Date(power$Date, "%d/%m/%Y")
+power <- subset(power, "2007-02-01" <= Date & "2007-02-02" >= Date)
+
+png(file="plot1.png")
+hist(power$Global_active_power, main="Global Active Power", xlab="Global Active Power (Killowats)", col="red")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/TompkinsD/Documents/coursera2019/r4")
+
+power <- read.csv("C:/Users/TompkinsD/Documents/coursera2019/r4/household_power_consumption.txt", sep=";", na.strings=c("?"))
+power$DateTime <- paste(power$Date,power$Time)
+power$DateTime <- strptime(power$DateTime, "%d/%m/%Y %H:%M:%S")
+power$Date <- as.Date(power$Date, "%d/%m/%Y")
+power <- subset(power, "2007-02-01" <= Date & "2007-02-02" >= Date)
+
+png(file="plot3.png")
+with(power, plot(DateTime, Sub_metering_1, pch=NA, xlab=" ", ylab = "Energy sub metering"))
+lines(power$DateTime, power$Sub_metering_1)
+lines(power$DateTime, power$Sub_metering_2, col="red")
+lines(power$DateTime, power$Sub_metering_3, col="blue")
+legend("topright", lty=1, col=c("black","red","blue"), legend = c("sub_metering_1","sub_metering_2","sub_metering_3"))
+dev.off()
